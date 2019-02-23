5dea8e161a3cd98c96bf4b94cc4cc1f1a40562c6
michaelcoleman7 <michaelcoleman135@gmail.com>
2019-02-23 19:46:32 +0000

    Setup Project with Grid colums and rows

---
+++ Sports_Score_Tracker/Sports_Score_Tracker/Sports_Score_Tracker/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Sports_Score_Tracker
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            SetupDefaults();
+        }
+
+        private void SetupDefaults()
+        {
+            var assembly = typeof(MainPage);
+            //string backgroundImgPath = "Sports_Score_Tracker.Assets.Images.background.jpg";
+            //backGrdImg.Source = ImageSource.FromResource(backgroundImgPath, assembly);
+
+            string soccerImgPath = "Sports_Score_Tracker.Assets.Images.soccer.jpg";
+            imgSoccer.Source = ImageSource.FromResource(soccerImgPath, assembly);
+
+            string basketballImgPath = "Sports_Score_Tracker.Assets.Images.basketball.jpg";
+            imgBasketball.Source = ImageSource.FromResource(basketballImgPath, assembly);
+
+            string tennisImgPath = "Sports_Score_Tracker.Assets.Images.tennisball.png";
+            imgTennis.Source = ImageSource.FromResource(tennisImgPath, assembly);
+
+            string hockeyImgPath = "Sports_Score_Tracker.Assets.Images.icehockey.PNG";
+            imgHockey.Source = ImageSource.FromResource(hockeyImgPath, assembly);
+
+            string gaaImgPath = "Sports_Score_Tracker.Assets.Images.gaa.jpg";
+            imgGaa.Source = ImageSource.FromResource(gaaImgPath, assembly);
+
+            string rugbyImgPath = "Sports_Score_Tracker.Assets.Images.rugby.jpg";
+            imgRugby.Source = ImageSource.FromResource(rugbyImgPath, assembly);
+        }
+    }
+}
