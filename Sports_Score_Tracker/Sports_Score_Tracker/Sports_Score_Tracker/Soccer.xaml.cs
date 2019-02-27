9d8700c6e5609d7312be7c355862856839be541f
michaelcoleman7 <michaelcoleman135@gmail.com>
2019-02-27 10:00:57 +0000

    Created base Soccer page and added elements
    created soccer page and added name, buttons and score labels etc...

---
+++ Sports_Score_Tracker/Sports_Score_Tracker/Sports_Score_Tracker/Soccer.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Sports_Score_Tracker
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Soccer : ContentPage
+	{
+		public Soccer ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
