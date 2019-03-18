eea5f242187ce09281e463e1d84c3bf482f3d4bc
Michael Coleman <michaelcoleman135@gmail.com>
2019-03-18 15:39:19 +0000

    Implemented Score incrementing
    added ability for users to increase the score of the home or away team

--- Sports_Score_Tracker/Sports_Score_Tracker/Sports_Score_Tracker/Soccer.xaml.cs
+++ Sports_Score_Tracker/Sports_Score_Tracker/Sports_Score_Tracker/Soccer.xaml.cs
@@ -16,5 +16,21 @@ namespace Sports_Score_Tracker
 		{
 			InitializeComponent ();
 		}
-	}
+
+        private void AddHome_Clicked(object sender, EventArgs e)
+        {
+            string score = homeScore.Text.ToString();
+            int addscore = Convert.ToInt32(score);
+            addscore = addscore + 1;
+            homeScore.Text = addscore.ToString();
+        }
+
+        private void AddAway_Clicked(object sender, EventArgs e)
+        {
+            string score = awayScore.Text.ToString();
+            int addscore = Convert.ToInt32(score);
+            addscore = addscore + 1;
+            awayScore.Text = addscore.ToString();
+        }
+    }
 }
