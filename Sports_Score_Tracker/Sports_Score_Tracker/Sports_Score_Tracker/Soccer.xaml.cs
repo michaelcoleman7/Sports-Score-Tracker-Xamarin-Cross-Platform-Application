357c97498a39746f231f965ea41962b34cb71be5
Michael Coleman <michaelcoleman135@gmail.com>
2019-03-18 20:32:43 +0000

    Added audio player
    Added soumds tot the app so when a button's pressed, sounds are played

--- Sports_Score_Tracker/Sports_Score_Tracker/Sports_Score_Tracker/Soccer.xaml.cs
+++ Sports_Score_Tracker/Sports_Score_Tracker/Sports_Score_Tracker/Soccer.xaml.cs
@@ -7,18 +7,24 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using SoccerClassNamespace;
+using Plugin.SimpleAudioPlayer;
+using System.IO;
 
 namespace Sports_Score_Tracker
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class Soccer : ContentPage
 	{
+        private ISimpleAudioPlayer _simpleAudioPlayer;
         List<SoccerClass> soccerList = new List<SoccerClass>();
 
         public Soccer ()
 		{
 			InitializeComponent ();
-		}
+            _simpleAudioPlayer = CrossSimpleAudioPlayer.CreateSimpleAudioPlayer();
+            Stream beepStream = GetType().Assembly.GetManifestResourceStream("Sports_Score_Tracker.AudioFiles.fulltime.mp3");
+            bool isSuccess = _simpleAudioPlayer.Load(beepStream);
+        }
 
         private void AddHome_Clicked(object sender, EventArgs e)
         {
@@ -39,9 +45,9 @@ namespace Sports_Score_Tracker
         private void SaveGame_Clicked(object sender, EventArgs e)
         {
             SoccerClass s = new SoccerClass(homeTeam.Text, homeScore.Text, awayTeam.Text, awayScore.Text);
-            s.HomeScore = "1";
             soccerList.Add(s);
             SoccerClass.SaveSoccerDataToFile(soccerList);
+            _simpleAudioPlayer.Play();
         }
     }
 }
