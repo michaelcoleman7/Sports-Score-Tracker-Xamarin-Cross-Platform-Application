31abe39f97e4e92390b222345122ea9dd04bc058
Michael Coleman <michaelcoleman135@gmail.com>
2019-03-29 15:48:44 +0000

    Added Hockey Page and its functionality

---
+++ Sports_Score_Tracker/ScoreTracker/ScoreTracker/IceHockey.xaml.cs
@@ -0,0 +1,59 @@
+﻿using Plugin.SimpleAudioPlayer;
+using ScoreTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ScoreTracker
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class IceHockey : ContentPage
+	{
+		public IceHockey ()
+		{
+			InitializeComponent ();
+		}
+        private ISimpleAudioPlayer audioPlayer;
+        List<MatchClass> hockeyList = new List<MatchClass>();
+
+        private void AddHome_Clicked(object sender, EventArgs e)
+        {
+            //Change homescore text to string then convert to an integer - add 1, then set to text property
+            string score = homeScore.Text.ToString();
+            int addscore = Convert.ToInt32(score);
+            addscore = addscore + 1;
+            homeScore.Text = addscore.ToString();
+        }
+
+        private void AddAway_Clicked(object sender, EventArgs e)
+        {
+            //Change awayscore text to string then convert to an integer - add 1, then set to text property
+            string score = awayScore.Text.ToString();
+            int addscore = Convert.ToInt32(score);
+            addscore = addscore + 1;
+            awayScore.Text = addscore.ToString();
+        }
+
+        private async void SaveGame_Clicked(object sender, EventArgs e)
+        {
+            //create new match class and add to hockeyList
+            MatchClass s = new MatchClass(homeTeam.Text, homeScore.Text, awayTeam.Text, awayScore.Text);
+            hockeyList.Add(s);
+            MatchClass.SaveMatchDataToFile(hockeyList);
+
+            //Add audio to application when game is saved - referenced from https://forums.xamarin.com/discussion/145050/beep-in-xamarin
+            audioPlayer = CrossSimpleAudioPlayer.CreateSimpleAudioPlayer();
+            Stream audioStream = GetType().Assembly.GetManifestResourceStream("ScoreTracker.AudioFiles.fulltime.mp3");
+            bool isSuccess = audioPlayer.Load(audioStream);
+            audioPlayer.Play();
+
+            await Navigation.PushAsync(new MainPage());
+        }
+    }
+}
