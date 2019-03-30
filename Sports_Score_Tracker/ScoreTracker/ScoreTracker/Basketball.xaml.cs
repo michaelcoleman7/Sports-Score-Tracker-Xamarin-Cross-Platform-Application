8d946b934f603b5ca1e41ddef3c5e57f992c14a8
Michael Coleman <michaelcoleman135@gmail.com>
2019-03-30 17:43:55 +0000

    created a basketball page and a base for implementing it

---
+++ Sports_Score_Tracker/ScoreTracker/ScoreTracker/Basketball.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Plugin.SimpleAudioPlayer;
+using ScoreTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ScoreTracker
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Basketball : ContentPage
+	{
+		public Basketball ()
+		{
+			InitializeComponent ();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+        private ISimpleAudioPlayer audioPlayer;
+        List<MatchClass> basketballList = new List<MatchClass>();
+
+        private void AddHome_Clicked(object sender, EventArgs e)
+        {
+            //Change homescore text to string then convert to an integer - add 1, then set to text property
+            string score = homeScore.Text.ToString();
+            int addscore = Convert.ToInt32(score);
+            addscore = addscore + 2;
+            homeScore.Text = addscore.ToString();
+        }
+
+        private void AddAway_Clicked(object sender, EventArgs e)
+        {
+            //Change awayscore text to string then convert to an integer - add 1, then set to text property
+            string score = awayScore.Text.ToString();
+            int addscore = Convert.ToInt32(score);
+            addscore = addscore + 2;
+            awayScore.Text = addscore.ToString();
+        }
+
+        private async void SaveGame_Clicked(object sender, EventArgs e)
+        {
+            //create new match class and add to hockeyList
+            MatchClass s = new MatchClass(gameType.Text, homeTeam.Text, homeScore.Text, awayTeam.Text, awayScore.Text);
+            basketballList.Add(s);
+            MatchClass.SaveMatchDataToFile(basketballList);
+
+            //Add audio to application when game is saved - referenced from https://forums.xamarin.com/discussion/145050/beep-in-xamarin
+            audioPlayer = CrossSimpleAudioPlayer.CreateSimpleAudioPlayer();
+            Stream audioStream = GetType().Assembly.GetManifestResourceStream("ScoreTracker.AudioFiles.BasketballBuzzer.mp3");
+            bool isSuccess = audioPlayer.Load(audioStream);
+            audioPlayer.Play();
+
+            //return to Mainpage
+            await Navigation.PushAsync(new MainPage());
+        }
+    }
+}
