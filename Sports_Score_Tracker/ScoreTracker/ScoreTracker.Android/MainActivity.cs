5ee9ff994bfa5aa31ba6bf745b5a6bfa0ca72d51
Michael Coleman <michaelcoleman135@gmail.com>
2019-04-06 21:04:18 +0100

    Updated android mainactivity to prevent landscape rotation
    Makes buttons go a bit out of sync and would have to re-adjust all buttons in all pages to incorporate this feature

--- Sports_Score_Tracker/ScoreTracker/ScoreTracker.Android/MainActivity.cs
+++ Sports_Score_Tracker/ScoreTracker/ScoreTracker.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace ScoreTracker.Droid
 {
-    [Activity(Label = "ScoreTracker", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "ScoreTracker", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
